8f1510b72448fc977901a0bbe5b6fb7116bdc8ed
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-23 15:38:20 +0530

    add if else, for loop, continue break

---
+++ 006_if_else.py
@@ -0,0 +1,25 @@
+my_age = 17
+
+if my_age > 17:
+    print("Adult")
+
+if my_age > 17:
+    print("Adult")
+else:
+    print("minor")
+
+if my_age > 17:
+    print("Adult")
+elif my_age == 17:
+    print("Turining Adult")
+else:
+    print("minor")
+
+
+my_age = 18
+msg = "Bye"
+if isinstance(my_age, int):
+    msg = "hello"
+
+print(msg)
+
---
+++ 007_for_loop.py
@@ -0,0 +1,15 @@
+for i in range(0, 5, 1):
+    print(i)
+
+my_list = [10, 20, 30, 40]
+for i in my_list:
+    print(i)
+
+for i in range(len(my_list)):
+    print(i)
+
+for i, j in enumerate(my_list):
+    print(i, j)
+
+for i in range(len(my_list)):
+    print(i, my_list[i])
