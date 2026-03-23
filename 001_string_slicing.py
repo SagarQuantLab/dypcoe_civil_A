1f9d6e294f9330e38a16890352167a3a68f47de5
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-23 14:55:33 +0530

    added string slicing

---
+++ 001_string_slicing.py
@@ -0,0 +1,29 @@
+my_string = "This is civil students"
+
+# access first element
+print(my_string[0])
+
+# access 'This'
+print(my_string[0:4])
+
+# access 'Ti'
+print(my_string[0:4:2])
+
+# last element
+print(my_string[-1])
+
+# reversed string
+print(my_string[::-1])
+
+# reverse students
+print(my_string[:-9:-1])
+
+# lower case 
+print(my_string.lower())
+
+# upper case
+print(my_string.upper())
+
+# replace string
+replace_string = my_string.replace("s", "X")
+print(replace_string)
